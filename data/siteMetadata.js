20dc53a2e433d00820330442e5a974ff54b2ff53
Aarav Hattangadi <aarav@hattangadi.com>
2023-06-13 21:48:55 +0530

    update mastodon link

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -16,7 +16,7 @@ const siteMetadata = {
   //facebook: 'https://facebook.com',
   youtube: 'https://youtube.com/@ThePianoKid',
   //linkedin: 'https://www.linkedin.com',
-  mastodon: 'https://dotnet.social/@ahattangadi',
+  mastodon: 'https://hachyderm.io/@ahattangadi',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
