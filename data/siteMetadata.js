6f7ff2b6ba0523e249c0125b644bb22333db1400
Aarav Hattangadi <aarav@hattangadi.com>
2023-06-26 17:03:23 +0530

    update favicon

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -9,7 +9,7 @@ const siteMetadata = {
   siteRepo: 'https://github.com/ahattangadi/weblog-core',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
-  socialBanner: '/static/images/twitter-card.png',
+  socialBanner: '',
   email: 'aarav@hattangadi.com',
   github: 'https://github.com/ahattangadi',
   twitter: 'https://twitter.com/a_hattangadi',
